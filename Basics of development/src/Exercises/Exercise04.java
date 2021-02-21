46b6a819c3b1ac6655ed3cf728bbe58b552c8f43
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-02-21 10:44:03 +0300

    Ex 4 is done

--- Basics of development/src/Exercises/Exercise04.java
+++ Basics of development/src/Exercises/Exercise04.java
@@ -1,7 +1,10 @@
 package Exercises;
 
+//Swap the fractional and integer parts of a number and display the resulting value of the number
 public class Exercise04 {
     public static void main(String[] args) {
-
+        double x = 456.789;
+        double y = (x * 1000) % 1000 + (int) x / 1000.0;
+        System.out.println(y);
     }
 }
