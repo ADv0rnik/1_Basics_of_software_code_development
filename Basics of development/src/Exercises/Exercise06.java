82a73f48813a9d0ed177ddce9acbea98aa453bb4
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-02-28 01:51:48 +0300

    Added exercise 05

---
+++ Basics of development/src/Exercises/Exercise06.java
@@ -0,0 +1,11 @@
+package Exercises;
+import java.lang.Math;
+
+public class Exercise06 {
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 5);
+        int y = (int) (Math.random() * 5);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
